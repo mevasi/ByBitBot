d9206e220e765cb892b337b78500fa96b418971b
mevasi <mevasi.com@gmail.com>
2021-11-04 01:27:01 +1000

    Not many changes, attempted redesign but did not pass testing. Committing last stable build. Basic menu kinda implemented. Some new features have slowed down execution. TBD fix speed and finish menu !TBD API keys!

--- trade.py
+++ trade.py
@@ -48,67 +48,69 @@ def check_position_balance(client, cmmnd):  # ensure difference between position
         elif buy_position < sell_position:
             return 0
 
-    return 0.01  # default order size if positions are equal
+    return 0.001  # default order size if positions are equal
+
+# TBD REPLACE ORDERS IF FILLED AND PRICE NOT CHANGING ^^
 
 
-def place(clnt, prc, quantity, ordr, command):  # manage buy or sell orders depending on which thread calls
+def place(clnt, prc, quantity, command):  # manage buy or sell orders depending on which thread calls
     # attempt to cancel all the orders
     # on this side, as the price has changed
     try:
-        for order in ordr:
-            print(clnt.LinearOrder.LinearOrder_cancel(symbol="BTCUSDT", order_id=order).result())
-        ordr = []
+        order_array = clnt.LinearOrder.LinearOrder_query(symbol="BTCUSDT").result()[0]['result']
+        for order in order_array:
+            if order['side'] == command:
+                print("Cancelling", command, "side order number", order['order_id'], "...", clnt.LinearOrder.
+                      LinearOrder_cancel(symbol="BTCUSDT", order_id=order['order_id']).result()[0]['ret_msg'])
     except:
         print("no open orders or cannot cancel")
 
     # try to place an order at the bid/ask, and a buy/sell to close order at the same price
     try:
-        ord_1 = clnt.LinearOrder.LinearOrder_new(side=command, symbol="BTCUSDT", order_type="Limit", qty=quantity,
-                                                 price=prc, time_in_force="PostOnly",
-                                                 reduce_only=False).result()[0]['result']['order_id']
-        print(ord_1)
-        ordr.append(ord_1)  # append new order to orders array
-        # ord_2 = clnt.LinearOrder.LinearOrder_new(side=command, symbol="BTCUSDT", order_type="Limit", qty=0.01,
-        #                                         price=prc, time_in_force="PostOnly",
-        #                                         reduce_only=True).result()[0]['result']['order_id']
-        # print(ord_2)
-        # ordr.append(ord_2)  # append new order to orders array
+        print(command, "order placed ...",
+              clnt.LinearOrder.LinearOrder_new(side=command, symbol="BTCUSDT", order_type="Limit",
+                                               qty=quantity,
+                                               price=prc, time_in_force="PostOnly",
+                                               reduce_only=False,
+                                               close_on_trigger=False).result()[0]['ret_msg'])
+        print(command, "to close placed ...",
+              clnt.LinearOrder.LinearOrder_new(side=command, symbol="BTCUSDT", order_type="Limit",
+                                               qty=0.001,
+                                               price=prc, time_in_force="PostOnly",
+                                               reduce_only=True,
+                                               close_on_trigger=True).result()[0]['ret_msg'])
     except:
         print("could not place one or both ", command, " side orders")
-    return ordr
+
+
+def bid_ask_intermediary(clnt, array, prc, commnd):
+    # if the price has not changed, do nothing. If it has, update my orders
+    if prc == Decimal(array[0]['price']):
+        return prc
+    else:
+        prc = Decimal(array[0]['price'])
+
+        # find the balance between open positions and place appropriate order
+        amount = check_position_balance(clnt, commnd)
+        if amount != 0:
+            place(clnt, prc, amount, commnd)
+        return prc
 
 
 def create_bids(client):  # thread for accessing globals bids, loops to place buy orders
-    # initial blank variables
+    # initial blank price of 0 to place first order
     price = 0
-    orders = []
-
-    # continuous loop in thread
+    # loop will place further orders as the price changes
     while 1:
-        # if the price has not changed, do nothing. If it has, update my orders
-        if price == Decimal(bids[0]['price']):
-            pass
-        else:
-            price = Decimal(bids[0]['price'])
-            amount = check_position_balance(client, "Buy")
-            if amount != 0:
-                orders = place(client, price, amount, orders, "Buy")
+        price = bid_ask_intermediary(client, bids, price, "Buy")
 
 
 def create_asks(client):  # thread for accessing globals asks, loops to place sell orders
-    # initial blank variables
+    # initial blank price of 0 to place first order
     price = 0
-    orders = []
-
-    # continuous loop in thread
+    # loop will place further orders as the price changes
     while 1:
-        # if the price has not changed, do nothing. If it has, update my orders
-        if price == Decimal(asks[0]['price']):
-            pass
-        else:
-            price = Decimal(asks[0]['price'])
-            amount = check_position_balance(client, "Sell")
-            orders = place(client, price, amount, orders, "Sell")
+        price = bid_ask_intermediary(client, asks, price, "Sell")
 
 
 def sub_menu():
@@ -116,8 +118,33 @@ def sub_menu():
     print("At any time")
     print("Press 1 to begin program")
     print("Press 2 to stop program")
-    print("Press 0 to quit")
-    # ans = input("- - - - - - - -\n")
+    print("Press any other key to quit")
+    ans = input("- - - - - - - -\n")
+
+    if ans == "1":
+        os.system('cls')
+        # initialise client with keys
+        by_client = bybit.bybit(test=False, api_key=api_key, api_secret=api_secret)
+
+        # define threads to keep order book updated and submit orders
+        book = threading.Thread(target=update_order_book, args=(by_client,))
+        buy = threading.Thread(target=create_bids, args=(by_client,))
+        sell = threading.Thread(target=create_asks, args=(by_client,))
+
+        # start threads
+        book.start()
+        time.sleep(5)  # wait for initial order book fill
+        buy.start()
+        sell.start()
+
+        # wait for updated input
+        while 1:
+            new_input = input()
+            print(new_input)
+    elif ans == "2":
+        print("TBD")
+    else:
+        return
 
 
 def main_menu():
@@ -129,6 +156,7 @@ def main_menu():
     print("Please make a selection and press Enter")
     ans = input("- - - - - - - -\n")
 
+    # read user input and take appropriate action
     if ans == "0":
         os.system('cls')
         print("TBD read API keys")
@@ -140,22 +168,6 @@ def main_menu():
 
 if __name__ == '__main__':
     # initialise main menu
-    # os.system('cls')
-    # menu = threading.Thread(target=main_menu)
-    # menu.start()
-
-    # time.sleep(10)
-
-    # initialise client with keys
-    by_client = bybit.bybit(test=True, api_key=api_key, api_secret=api_secret)
-
-    # define threads to keep order book updated and submit orders
-    book = threading.Thread(target=update_order_book, args=(by_client,))
-    buy = threading.Thread(target=create_bids, args=(by_client,))
-    sell = threading.Thread(target=create_asks, args=(by_client,))
-
-    # start threads
-    book.start()
-    time.sleep(5)  # wait for initial order book fill
-    buy.start()
-    sell.start()
+    os.system('cls')
+    menu = threading.Thread(target=main_menu)
+    menu.start()
